8f033b681bfc4ec14c0ec600edd5800c60f0a395
Vinícius <vinybf@hotmail.com>
2021-07-19 14:56:04 -0300

    novos graficos e dados

--- junta_csvs.py
+++ junta_csvs.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import glob
-import os
 from functools import reduce
 
-path = "dados/criptos_yahoo"
-os.chdir(path)
-nome_arquivos = glob.glob("*.csv")
+TIPO_DADOS = "criptos"
+
+path = f"dados\\{TIPO_DADOS}_yahoo\\"
+nome_arquivos = glob.glob(path+"*.csv")
 print(f"{len(nome_arquivos)} arquivos encontrados!")
 
 dfs = []
 for arquivo in nome_arquivos:
-    simbolo = arquivo.split(".")[0]
+    simbolo = arquivo.split("\\")[-1].split(".")[0]
 
     df = pd.read_csv(arquivo)[["Date", "Adj Close"]]
     df["Date"] = pd.to_datetime(df["Date"])
@@ -19,4 +19,4 @@ for arquivo in nome_arquivos:
 
 merged_df = reduce(lambda left, right: pd.merge(left, right, on=["Date"], how="left"), dfs)
 print(merged_df.head())
-merged_df.to_csv("dados_criptos.csv")
+merged_df.to_csv(f"dados_{TIPO_DADOS}.csv")
